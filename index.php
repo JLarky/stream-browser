342f0dc630a4d40ec220d45a5707012aacb0a929
JLarky <jlarky@jlarky.(none)>
2009-04-30 21:25:00 +0400

    Сделал похожим на сайт Журавлика :)

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<?php
-$servers=unserialize(file_get_contents("db/serverlist.db"));
-?><html>
+<html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title>Трансляции локальной сети</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script src="js/updateinfo.js"></script>
 	<style type="text/css">.online{color:green}.offline{color:red}</style>
@@ -27,8 +26,6 @@ $(document).ready(
   id=$(this).attr('id').substring(6)
   url='stream/status2.php?id='+id;
   $(this).html('<img src="stuff/loading.gif"> Загрузка').load(url)
-  //console.log()
-  //console.log(url)
  })
 })
 </script>
@@ -36,39 +33,41 @@ $(document).ready(
 
 <body>
 <div id="tooltip" style="position: absolute;display:none"></div>
-<div class="sresults">
-<div border="0" style="width:100%;border-bottom:1px solid black;margin-bottom:2pt;padding-bottom:2pt"><img alt="" src="stuff/shout.gif"/>&nbsp;<b>Трансляции </b><br></div>
+<div id="header"><img alt="" src="stuff/shout.gif"/>&nbsp;Трансляции</div>
 <h4>Если знаете еще сервера, пишите в личку в DC.</h4>Для просмотра картинки трансляции подведите мышку к слову <span style="color:#008000">Online</span>.<br /><br />
 <em>Новая функция:</em> описание трансляций. Уважаемый пользователь, есть просьба править описания по мере необходимости. Это несложно: нажмите на текущее описание, введите новый текст и щелкните "Обновить".
-<br /><br />
 
-<?php
-print("<table style=\"text-align:center;width:100%;\">
+<br /><br />
+<table>
 <tr>
 	<th>Состояние</th>
 	<th>Адрес</th>
 	<th>Держатель сервиса</th>
 	<th>Ретрансляция</th>
 	<th>Что транслирует</th> 
+
 </tr>
-");
+
+
+<?php
+$servers=unserialize(file_get_contents("db/serverlist.db"));
 
 foreach($servers as $i => $server)
 {
-	print('<tr>
+	print('<tr class="tr'.($i & 1).'">
 		<td>
 			<span id="stream'.$i.'" class="indicator"></span>
 		</td>
-		<td><a href="'.$server['url'].'">'.$server['url'].'</a></td>
-		<td>'.$server['owner'].'</td>
+		<td class="url"><a href="'.$server['url'].'">'.$server['url'].'</a></td>
+		<td class="owner">'.$server['owner'].'</td>
 		<td>'.htmlspecialchars($server['retrans']).'</td>
 		');
 
 	$trans = ($server['desc'] ? $server['desc'] : "Нет описания");
 
-	print('<td><div id="infobox'.$i.'"><a style="text-decoration:underline;" onclick="document.getElementById(\'infobox'.$i.'\').style.display=\'none\';document.getElementById(\'editbox'.$i.'\').style.display=\'block\';return false;">'.$trans.'</a></div><div id="editbox'.$i.'" style="display:none;"><input type="text" value="'.$trans.'" id="stext'.$i.'" /><a onclick="updateInfo(\''.$i.'\')">Обновить</a></div></td>');
+	print('<td class="desc"><div id="infobox'.$i.'"><a class="slabel" onclick="document.getElementById(\'infobox'.$i.'\').style.display=\'none\';document.getElementById(\'editbox'.$i.'\').style.display=\'block\';return false;">'.$trans.'</a></div><div id="editbox'.$i.'" style="display:none;"><input type="text" value="'.$trans.'" id="stext'.$i.'" /><a onclick="updateInfo(\''.$i.'\')">Обновить</a></div></td>');
 
-	print('</tr>');
+	print("</tr>\n\n");
 }
 print("</table>");
 ?>
@@ -76,7 +75,7 @@ print("</table>");
 </div>
 
 <div style="text-align:center">
-Примечание от JLarky: если вам интересно как плохо написана эта страничка и вам охото её переписать, напишите мне в DC или jlarky@gmail.com
+<a href="http://github.com/JLarky/stream-browser/tree/master">view source</a>
 </div>
 
 </body>
--- js/ak.php
+++
@@ -1,7 +0,0 @@
-<?php
-Header("Expires: ".date("D, j M Y H:i:s",time()+(86400 *30))." UTC");
-Header("Cache-Control: Public");
-Header("Pragma: Public");
-
-readfile('ak.js');
-?>
